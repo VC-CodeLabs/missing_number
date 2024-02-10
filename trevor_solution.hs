8a30afb8502d6a8a5b52043ccc808b9c4c286a66
merestar <105946788+merestar@users.noreply.github.com>
2024-02-10 00:42:56 -0500

    MissingNumber algorithm

--- trevor_solution.hs
+++ trevor_solution.hs
@@ -1,23 +1,16 @@
 module Main where
 
 main = do
-  putStrLn "Hello, World!"
-  print nums
-  putStrLn $ show (minMaxSum nums)
-  print (missingNumber nums)
+  print $ missingNumber nums1
+  print $ missingNumber nums2
+  print $ missingNumber nums3
+  print $ missingNumber nums4
+  
+nums1 = [3, 7, 1, 2, 8, 4, 5]
+nums2 = [11, 2, 10, 4, 5, 6, 7, 8, 1, 9]
+nums3 = [4, 3, 1, 5]
+nums4 = [23, 24, 22, 26, 28, 21, 20, 27, 19, 32, 31, 30, 29]
 
-nums = [3, 7, 1, 2, 8, 4, 5]
-
-
-minMaxSum lst = minMaxSum' (lst !! 0) (lst !! 0) 0 lst where
-    minMaxSum' a b c [] = [a, b, c]
-    minMaxSum' a b c (x : xs) = minMaxSum' (if x < a then x else a) (if x > b then x else b) (x + c) xs
-
-missingNumber :: [Int] -> Int
-missingNumber lst = do
-    let r = minMaxSum lst
-    let a = r !! 0
-    let b = r !! 1
-    let c = r !! 2
-    return (2 * a + (b - a)) * (b - a + 1) / 2 - c
-    
+missingNumber lst = missingNumber' (lst !! 0) (lst !! 0) 0 lst where
+    missingNumber' a b c [] = (floor ((2 * a + (b - a)) * (b - a + 1) / 2 - c))
+    missingNumber' a b c (x : xs) = missingNumber' (if x < a then x else a) (if x > b then x else b) (x + c) xs
